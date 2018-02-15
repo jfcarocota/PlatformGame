181cc5c1c051a20070f5bb10cc2e8098ff0ae4b2
jfcarocota <jfcarocota@gmail.com>
2018-02-15 15:07:26 -0700

    Clase15/02/2018
    Clases abstractas, metodos virtuales y fisica.

---
+++ Assets/_Scripts/Character.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Animator))]
+
+public abstract class Character : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody2D rb2D;
+
+    [SerializeField]
+    float moveSpeed;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rb2D.freezeRotation = true;
+    }
+
+    protected virtual void MovePlayer()
+    {
+        transform.Translate(Vector2.right * Axis.x * moveSpeed * Time.deltaTime);
+    }
+
+    /*protected abstract void MovePlayerAbstractVer();
+
+    protected abstract void Jump();
+
+    protected abstract void Attack();
+
+    protected abstract void Die();*/
+
+
+    Vector2 Axis
+    {
+        
+        get { return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); }
+    }
+}
---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : Character
+{
+    protected override void MovePlayer()
+    {
+        Debug.Log("My Version of move player");
+        base.MovePlayer();
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+}
