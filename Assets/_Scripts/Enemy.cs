38e8d3fa02e2bf32f7beedb7be4ec66a2e70e289
jfcarocota <jfcarocota@gmail.com>
2018-03-05 15:51:09 -0700

    clase 05/03/2018

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    IEnumerator patrol;
+
+    private void Start()
+    {
+        patrol = Patrolling();
+        StartCoroutine(patrol);
+    }
+
+    IEnumerator Patrolling()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+            Debug.Log("Hello after 5 seconds");
+        }
+    }
+}
