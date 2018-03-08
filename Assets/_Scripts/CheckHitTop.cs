6c306b8c5cc92005cd641dbfb33ea09722aca403
jfcarocota <jfcarocota@gmail.com>
2018-03-08 13:29:17 -0700

    Hip top added

---
+++ Assets/_Scripts/CheckHitTop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckHitTop : MonoBehaviour {
+
+    [SerializeField]
+    Enemy enemey;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            enemey.Bc2D.enabled = false;
+        }
+    }
+}
--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -6,19 +6,84 @@ public class Enemy : MonoBehaviour
 {
 
     IEnumerator patrol;
+    IEnumerator moveRight;
+    IEnumerator moveLeft;
+    [SerializeField]
+    float delay;
+    [SerializeField]
+    float speedPatrol;
+    enum Direction{right, left }
+    [SerializeField]
+    Direction direction;
+
+    Rigidbody2D rb2D;
+    BoxCollider2D bc2D;
+
+    public BoxCollider2D Bc2D
+    {
+        get
+        {
+            return bc2D;
+        }
+    }
+
+    public Rigidbody2D Rb2D
+    {
+        get
+        {
+            return rb2D;
+        }
+    }
+
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        bc2D = GetComponent<BoxCollider2D>();
+    }
 
     private void Start()
     {
-        patrol = Patrolling();
+        patrol = Patrolling(delay);
+        moveRight = Moving(Vector2.right,0);
+        moveLeft = Moving(Vector2.left, 0);
         StartCoroutine(patrol);
     }
 
-    IEnumerator Patrolling()
+    IEnumerator Patrolling(float delay)
     {
         while (true)
         {
-            yield return new WaitForSeconds(5f);
-            Debug.Log("Hello after 5 seconds");
+            if(direction == Direction.right)
+            {
+               StartCoroutine( moveRight);
+            }
+            if (direction == Direction.left)
+            {
+               StartCoroutine(moveLeft);
+            }
+            yield return new WaitForSeconds(delay);
+            switch (direction)
+            {
+                case Direction.right:
+                    StopCoroutine(moveRight);
+                    direction = Direction.left;
+                    break;
+                case Direction.left:
+                    StopCoroutine(moveLeft);
+                    direction = Direction.right;
+                    break;
+
+            }
+           
+        }
+    }
+
+    IEnumerator Moving(Vector2 movDir ,float delay)
+    {
+        while (true)
+        {
+            transform.Translate(movDir * speedPatrol * Time.deltaTime);
+            yield return new WaitForSeconds(delay);
         }
     }
 }
